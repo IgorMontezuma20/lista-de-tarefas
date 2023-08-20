e670f0c1b86e35dbe76430ce0a8d0fcdb48768ba
IgorMontezuma20 <igorminranda17@gmail.com>
2023-08-20 11:35:07 -0300

    feat: registrando tarefas

--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -1,15 +1,42 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { signOut } from "firebase/auth";
-import { auth } from "../../firebaseConnection";
+import { auth, db } from "../../firebaseConnection";
+import { addDoc, collection } from "firebase/firestore";
 
 import "./admin.css";
 
 export default function Admin() {
   const [tarefaInput, setTarefaInput] = useState("");
+  const [user, setUser] = useState({});
 
-  function handleRegister(e) {
+  useEffect(() => {
+    async function loadTarefas() {
+      const userDetail = localStorage.getItem("@detailUser");
+      setUser(JSON.parse(userDetail));
+    }
+
+    loadTarefas();
+  }, []);
+
+  async function handleRegister(e) {
     e.preventDefault();
-    alert("Clicou");
+    if (tarefaInput === "") {
+      alert("Digite sua tarefa...");
+      return;
+    }
+
+    await addDoc(collection(db, "tarefas"), {
+      tarefa: tarefaInput,
+      created: new Date(),
+      userUid: user?.uid,
+    })
+      .then(() => {
+        console.log("Tarefa Registrada!");
+        setTarefaInput("");
+      })
+      .catch((err) => {
+        console.log("Erro ao registrar tarefa" + err.message);
+      });
   }
 
   async function handleLogout() {
