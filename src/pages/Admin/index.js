f7857c1ae046d3b173760415adf9ae8820a6a2b1
IgorMontezuma20 <igorminranda17@gmail.com>
2023-08-20 12:24:33 -0300

    feat: listando tarefas

--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -8,6 +8,8 @@ import {
   query,
   orderBy,
   where,
+  doc,
+  deleteDoc,
 } from "firebase/firestore";
 
 import "./admin.css";
@@ -43,7 +45,6 @@ export default function Admin() {
             });
           });
 
-          console.log(lista);
           setTarefas(lista);
         });
       }
@@ -77,6 +78,11 @@ export default function Admin() {
     await signOut(auth);
   }
 
+  async function deleteTarefa(id) {
+    const docRef = doc(db, "tarefas", id);
+    await deleteDoc(docRef);
+  }
+
   return (
     <div className="admin-container">
       <h1>Minhas tarefas</h1>
@@ -93,14 +99,21 @@ export default function Admin() {
         </button>
       </form>
 
-      <article className="list">
-        <p>Estudar JavaScript e React</p>
+      {tarefas.map((item) => (
+        <article key={item.id} className="list">
+          <p>{item.tarefa}</p>
 
-        <div>
-          <button>Editar</button>
-          <button className="btn-delete">Concluir</button>
-        </div>
-      </article>
+          <div>
+            <button>Editar</button>
+            <button
+              onClick={() => deleteTarefa(item.id)}
+              className="btn-delete"
+            >
+              Concluir
+            </button>
+          </div>
+        </article>
+      ))}
 
       <button className="btn-logout" onClick={handleLogout}>
         Sair
